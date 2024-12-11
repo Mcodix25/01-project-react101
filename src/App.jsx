16f0ac572988f867b4c4b605bd659cc7a334d84f
mcrod <paviamcroddixon14@gmail.com>
2024-12-11 13:21:34 +0800

    working to, gumagana button nag output sa U.I

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 
-
+import {useState} from 'react';
 import {CORE_CONCEPTS} from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcepts_ from "./components/CoreConcepts_.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  const [selectedTopic,setSelectedTopic]= useState('Please click button');
 
-  function handleSelect(){
-    console.log('Hello McRod + handleSellect is running');
+  function handleSelect(selectedButton){
+   // console.log('Hello McRod + handleSellect is running');
+   //tabContent=selectedButton;
+   setSelectedTopic(selectedButton);
+   console.log(selectedTopic);
 }
+
+console.log('APP COMPONENT EXECUTING');
 
   return (
     <div>
@@ -34,12 +40,12 @@ function App() {
           <h2>Examples</h2>
           <menu>
            
-            <TabButton onSelect = {handleSelect}> Components </TabButton>
-            <TabButton onSelect = {handleSelect}> JSX </TabButton>
-            <TabButton onSelect = {handleSelect}> Props </TabButton>
-            <TabButton onSelect = {handleSelect}> State </TabButton>
+            <TabButton onSelect = {()=>handleSelect('Components Selected')}> Components </TabButton>
+            <TabButton onSelect = {()=>handleSelect("JSX Selected")}> JSX </TabButton>
+            <TabButton onSelect = {()=>handleSelect('Props Selected men!')}> Props </TabButton>
+            <TabButton onSelect = {()=>handleSelect("Selected State!")}> State </TabButton>
           </menu>
-          Dynamic Content
+          {selectedTopic}
 
         </section>
       </main>
